4a3e4f6a21fe69afd3d4335b7d1c943b52eb8b6a
mpi2010 <mpi@mpi.biz.ua>
2020-05-15 12:32:19 +0300

    Modul 15

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,8 +4,15 @@ namespace App\Exceptions;
 
 use App\Traits\ApiResponser;
 use Exception;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -56,7 +63,43 @@ class Handler extends ExceptionHandler
         if($exception instanceof ValidationException){
             return $this->convertValidationExceptionToResponse($exception,$request);
         }
-        return parent::render($request, $exception);
+        if($exception instanceof ModelNotFoundException)
+        {
+            $modelName = strtolower(class_basename($exception->getModel()));
+
+            return  $this->errorResponse("Does not exists any {$modelName} with the specified identificator", 404);
+        }
+        if($exception instanceof AuthenticationException)
+        {
+            return  $this->unauthenticated($request, $exception) ;
+        }
+        if($exception instanceof AuthorizationException)
+        {
+            return  $this->errorResponse($exception->getMessage(), 403);
+        }
+        if($exception instanceof NotFoundHttpException)
+        {
+            return  $this->errorResponse('The specified URL cannot be found', 404);
+        }
+        if($exception instanceof MethodNotAllowedHttpException)
+        {
+            return  $this->errorResponse('The specified  method for the request is invalid', 405);
+        }
+        if($exception instanceof HttpException)
+        {
+            return  $this->errorResponse($exception->getMessage(), $exception->getStatusCode());
+        }
+        if($exception instanceof QueryException)
+        {
+            $errorCode = $exception->errorInfo[1];
+            if ($errorCode == 1451) {
+                return $this->errorResponse('Cannot remove this resource permanently. It is related with any other resources.', 409);
+            }
+        }
+        if(config('app.debug')){
+            return parent::render($request, $exception);
+        }
+        $this->errorResponse('Unexpected Exception.Try later', 500);
     }
 
     /**
@@ -73,4 +116,15 @@ class Handler extends ExceptionHandler
         return $this->errorResponse($errors,422);
     }
 
+    /**
+     * Convert an authentication exception into a response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Auth\AuthenticationException  $exception
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        return $this->errorResponse('Unauthenticated.', 401);
+    }
 }
--- app/User.php
+++ app/User.php
@@ -3,13 +3,14 @@
 namespace App;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, SoftDeletes;
 
     const ADMIN_USER = 'true';
     const REGULAR_USER = 'false';
@@ -18,7 +19,7 @@ class User extends Authenticatable
     const UNVERIFIED_USER = '0';
 
     protected $table = 'users';
-
+    protected $dates = ['deleted_at'];
     /**
      * The attributes that are mass assignable.
      *
